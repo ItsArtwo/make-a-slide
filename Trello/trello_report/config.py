860ebd06c7955312a8b3301115f127437145c3b1
Arthur Miolla Dias <arthurmiolla@hotmail.com>
2025-08-27 22:05:39 -0300

    Delete Trello directory

--- Trello/trello_report/config.py
+++
@@ -1,9 +0,0 @@
-API_KEY = 'a2ffb6570256b8afa8d0262596294e36'
-TOKEN = 'ATTA71a7e45fb51ca2bd4c24dd8d44a032c98a778e0ff331eb6f70130e3ec24e8a31AAFA42DF'
-BOARD_ID = '66ddcdc0176ed00c5c401bb5'
-LIST_IDS = {
-    "pendente": '66ddcdc0176ed00c5c401bbc',
-    "andamento": '66ddcdc0176ed00c5c401bbd',
-    "concluido": '66ddcdc0176ed00c5c401bbe',
-    "correcao": '66ddd0f27a2d8828175760d8'
-}
